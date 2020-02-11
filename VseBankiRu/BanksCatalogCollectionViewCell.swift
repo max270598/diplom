f29c6851653b91f3b95d1a509f4a9a0d48868678
Max <Olyushkin.MK@Sberleasing.ru>
2020-02-11 10:09:29 +0300

    Initial Commit

---
+++ VseBankiRu/BanksCatalogCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BanksCatalogCollectionViewCell.swift
+//  VseBankiRu
+//
+//  Created by Мах Ol on 2/7/20.
+//  Copyright © 2020 Мах Ol. All rights reserved.
+//
+
+import UIKit
+
+class BanksCatalogCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var backImageView: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ VseBankiRu/BanksCatalogModelCell.swift
@@ -0,0 +1,8 @@
+//
+//  BanksCatalogModel.swift
+//  VseBankiRu
+//
+//  Created by Мах Ol on 2/7/20.
+//  Copyright © 2020 Мах Ol. All rights reserved.
+//
+
